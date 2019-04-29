71b69399fb255c0dafc5fed341889f6fc5338fb8
Vyacheslav Kalinkin <inpego@bongord.com>
2019-04-30 00:53:52 +0500

    Add attr_readers.

--- lib/processor.rb
+++ lib/processor.rb
@@ -2,6 +2,8 @@
 
 module PingStats
   class Processor
+    attr_reader :server
+
     def self.restart_all
       Server.where(enabled: true).each do |server|
         new(server: server).start if `ps ax | grep '[p]ing #{server.ip_address}'`.empty?
@@ -14,12 +16,12 @@ module PingStats
 
     def start
       collector_path = File.join(__dir__, '../scripts/collector.rb')
-      collector_cmd = "~/.rvm/bin/rvm default do bundle exec ruby #{collector_path} #{ENV['RACK_ENV']} #{@server.id}"
-      system("(ping #{@server.ip_address} 2>&1 | #{collector_cmd} &)")
+      collector_cmd = "~/.rvm/bin/rvm default do bundle exec ruby #{collector_path} #{ENV['RACK_ENV']} #{server.id}"
+      system("(ping #{server.ip_address} 2>&1 | #{collector_cmd} &)")
     end
 
     def stop
-      `kill $(ps ax | grep '[c]ollector.rb #{ENV['RACK_ENV']} #{@server.id}' | awk '{ print $1 }')`
+      `kill $(ps ax | grep '[c]ollector.rb #{ENV['RACK_ENV']} #{server.id}' | awk '{ print $1 }')`
     end
   end
 end
--- services/add_service.rb
+++ services/add_service.rb
@@ -2,13 +2,15 @@
 
 module PingStats
   class AddService
+    attr_reader :ip_address
+
     def initialize(ip_address)
       @ip_address = ip_address
     end
 
     def call
-      Server.update_or_create({ ip_address: @ip_address }, enabled: true)
-      Processor.new(ip_address: @ip_address).start
+      Server.update_or_create({ ip_address: ip_address }, enabled: true)
+      Processor.new(ip_address: ip_address).start
     end
   end
 end
